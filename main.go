c01e2187aa69a744696b534e8ca2816146b74a17
pprobst <pprobst@insiberia.net>
2019-09-17 13:59:44 -0300

    if err...

--- main.go
+++ main.go
@@ -4,14 +4,30 @@ import (
 	"fmt"
 	"github.com/pprobst/beatrice/src/config"
 	"github.com/pprobst/beatrice/src/generator"
+	"os"
 )
 
 func main() {
-	cfg, _ := config.ReadConfig()
+	if err := generateHTML(); err != nil {
+		fmt.Errorf("Error: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("Fin.")
+}
+
+func generateHTML() error {
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		return fmt.Errorf("ERROR reading config file: %v", err)
+	}
 	posts := generator.GetPosts(cfg)
 
-	generator.GenerateIndexHTML(cfg, &posts)
-	generator.GeneratePostsHTML(cfg, posts)
+	if err := generator.GenerateIndexHTML(cfg, &posts); err != nil {
+		return fmt.Errorf("ERROR generating index HTML: %v", err)
+	}
+	if err := generator.GeneratePostsHTML(cfg, posts); err != nil {
+		return fmt.Errorf("ERROR generating posts HTML: %v", err)
+	}
 
-	fmt.Println("Fin.")
+	return nil
 }
